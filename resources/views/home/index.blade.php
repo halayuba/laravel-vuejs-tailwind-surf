a8d81524b6a0f29bf162feb14f3231f2934949e0
Simon Bashir <simon_bashir@yahoo.com>
2021-03-17 14:14:27 -0500

    mobile design app name fix

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,10 +1,10 @@
 <x-app-layout>
   <x-alerts />
 
-  <div class="mt-4 xl:mt-12 max-w-7xl mx-auto px-2 py-4 sm:p-6 xl:p-8 bg-white text-gray-700">
+  <div class="mt-4 xl:mt-12 max-w-7xl mx-auto px-4 py-4 sm:p-6 xl:p-8 bg-white text-gray-700">
     Developed using Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
 
-    <div class="mt-4 xl:mt-8 text-3xl text-indigo-500">
+    <div class="mt-4 xl:mt-8 text:text-xl lg:text-3xl text-indigo-500">
       This is a "demo" project but contains "real" data gathered from various resources and the main focus is on learning Laravel, Vuejs, and Tailwind.
     </div>
   </div>
